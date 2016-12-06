8757324a824fae206c895c325b1bd14051cc64fe
Markus Bohl <markus.bohl@gmail.com>
2016-12-07 00:11:57 +0100

    Enable AbstractValidator to actually validate objects

--- src/AbstractValidator.spec.ts
+++ src/AbstractValidator.spec.ts
@@ -3,21 +3,120 @@
 "use strict";
 
 import { AbstractValidator } from "./AbstractValidator";
+import { Severity } from "./validation/Severity";
+import { ValidationResult } from "./validation/ValidationResult";
+import { ValidationFailure } from "./validation/ValidationFailure";
+
+let validationFailure: ValidationFailure = null;
 
 class TestValidator extends AbstractValidator<TestPerson> {
     constructor() {
         super();
-        this.ruleFor((TestPerson) => { return name; }).isNotNull().withErrorCode("C-3628/B");
+        this.ruleFor((input: TestPerson) => { return input.name; }).isNotNull()
+            .withErrorCode("C-3628/B");
+        this.ruleFor((input: TestPerson) => { return input.age; }).isEqualTo(18)
+            .withErrorMessage("not eighteen").withSeverity(Severity.INFO);
+        this.ruleFor((input: TestPerson) => { return input.address; }).isEmpty()
+            .onFailure((failure) => {
+                validationFailure = failure;
+            });
     }
 }
 
 class TestPerson {
     name: string;
     age: number;
+    address: string;
 }
 
 describe("AbstractValidator", () => {
-    describe("ruleFor()", () => {
+    describe("TestValidator.validate()", () => {
+        let validator: TestValidator;
 
+        beforeEach(() => {
+            validationFailure = null;
+            validator = new TestValidator();
+        });
+
+        it("should return a positive validation result for a valid instance", () => {
+            let person = new TestPerson();
+            person.name = "Franz";
+            person.age = 18;
+
+            let result: ValidationResult = validator.validate(person);
+
+            expect(result.isValid()).toBeTruthy();
+        });
+
+        it("should return a negative validation result if name is null", () => {
+            let person = new TestPerson();
+            person.name = null;
+            person.age = 18;
+
+            let result: ValidationResult = validator.validate(person);
+
+            expect(result.isValid()).toBeFalsy();
+        });
+
+        it("should return a proper failure-object if name is null", () => {
+            let person = new TestPerson();
+            person.name = null;
+            person.age = 18;
+
+            let failure: ValidationFailure = validator.validate(person).getFailures()[0];
+
+            expect(failure.target).toBe(person);
+            expect(failure.propertyName).toBe("name");
+            expect(failure.errorCode).toBe("C-3628/B");
+            expect(failure.severity).toBe(Severity.ERROR);
+            expect(failure.errorMessage).not.toBeDefined();
+        });
+
+        it("should return a negative validation result if age is not 18", () => {
+            let person = new TestPerson();
+            person.name = "Franz";
+            person.age = 21;
+
+            let result: ValidationResult = validator.validate(person);
+
+            expect(result.isValid()).toBeFalsy();
+        });
+
+        it("should return a proper failure-object if age is not 18", () => {
+            let person = new TestPerson();
+            person.name = "Franz";
+            person.age = 21;
+
+            let failure: ValidationFailure = validator.validate(person).getFailures()[0];
+
+            expect(failure.target).toBe(person);
+            expect(failure.propertyName).toBe("age");
+            expect(failure.errorCode).not.toBeDefined();
+            expect(failure.severity).toBe(Severity.INFO);
+            expect(failure.errorMessage).toBe("not eighteen");
+        });
+
+        it("should return one failure for every validation that failed", () => {
+            let person = new TestPerson();
+            person.name = null;
+            person.age = 21;
+
+            let result: ValidationResult = validator.validate(person);
+
+            expect(result.getFailures().length).toBe(2);
+        });
+
+        it("should trigger callback on failure", () => {
+            let person = new TestPerson();
+            person.name = "Franz";
+            person.age = 18;
+            person.address = "my address";
+
+            validator.validate(person);
+
+            expect(validationFailure).not.toBeNull();
+            expect(validationFailure.target).toBe(person);
+            expect(validationFailure.propertyName).toBe("address");
+        });
     });
 });
--- src/AbstractValidator.ts
+++ src/AbstractValidator.ts
@@ -2,10 +2,33 @@
 
 import { ValidationOptionsBuilder } from "./builder/ValidationOptionsBuilder";
 import { ValidatorBuilder } from "./builder/ValidatorBuilder";
+import { ValidatorBuilderImpl } from "./builder/ValidatorBuilderImpl";
+import { ValidationResult } from "./validation/ValidationResult";
+import { ValidationRule } from "./validation/ValidationRule";
 
 export abstract class AbstractValidator<T> {
 
+    private rules: any[] = [];
+
     protected ruleFor<T, TProperty>(lambdaExpression: (input: T) => TProperty): ValidatorBuilder<T, TProperty> {
-        return null;
+        let rule: ValidationRule<T, TProperty> = new ValidationRule(lambdaExpression);
+        this.rules.push(rule);
+
+        return new ValidatorBuilderImpl<T, TProperty>(rule);
+    }
+
+    validate(input: T): ValidationResult {
+        let result = new ValidationResult();
+
+        this.rules.forEach((element: any) => {
+            let rule = element as ValidationRule<T, any>;
+            let outcome = rule.apply(input);
+
+            if (outcome.isFailure()) {
+                result.addFailure(outcome.getValidationFailure());
+            }
+        });
+
+        return result;
     }
 }
