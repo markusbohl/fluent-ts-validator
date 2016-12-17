035dad09e42d653f5e9c3bd3b2197c74b6c9723f
Markus Bohl <markus.bohl@gmail.com>
2016-12-17 14:11:17 +0100

    Reduce cyclomatic complexity in AbstractValidator

--- src/AbstractValidator.ts
+++ src/AbstractValidator.ts
@@ -21,7 +21,7 @@ import {
 
 /**
  * Abstract base class for all custom validators.
- * 
+ *
  * @export
  * @abstract
  * @class AbstractValidator
@@ -29,13 +29,13 @@ import {
  */
 export abstract class AbstractValidator<T> implements Validatable<T> {
 
-    private rules: any[] = [];
+    private rules: ValidationRule<T, any>[] = [];
 
-    protected ruleFor<T>(lambdaExpression: (input: T) => number): CommonValidatorBuilder<T, number> & NumberValidatorBuilder<T>;
-    protected ruleFor<T>(lambdaExpression: (input: T) => Date): CommonValidatorBuilder<T, Date> & DateValidatorBuilder<T>;
-    protected ruleFor<T>(lambdaExpression: (input: T) => string): CommonValidatorBuilder<T, string> & StringValidatorBuilder<T>;
-    protected ruleFor<T, TProperty>(lambdaExpression: (input: T) => TProperty): CommonValidatorBuilder<T, TProperty>;
-    protected ruleFor<T, TProperty>(lambdaExpression: (input: T) => TProperty): ValidatorBuilder<T, TProperty> {
+    protected ruleFor(lambdaExpression: (input: T) => number): CommonValidatorBuilder<T, number> & NumberValidatorBuilder<T>;
+    protected ruleFor(lambdaExpression: (input: T) => Date): CommonValidatorBuilder<T, Date> & DateValidatorBuilder<T>;
+    protected ruleFor(lambdaExpression: (input: T) => string): CommonValidatorBuilder<T, string> & StringValidatorBuilder<T>;
+    protected ruleFor<TProperty>(lambdaExpression: (input: T) => TProperty): CommonValidatorBuilder<T, TProperty>;
+    protected ruleFor<TProperty>(lambdaExpression: (input: T) => TProperty): ValidatorBuilder<T, TProperty> {
         let rule: ValidationRule<T, TProperty> = new ValidationRule(lambdaExpression);
         this.rules.push(rule);
 
@@ -45,8 +45,7 @@ export abstract class AbstractValidator<T> implements Validatable<T> {
     validate(input: T): ValidationResult {
         let result = new ValidationResult();
 
-        this.rules.forEach((element: any) => {
-            let rule = element as ValidationRule<T, any>;
+        this.rules.forEach((rule: any) => {
             let outcome = rule.apply(input);
 
             if (outcome.isFailure()) {
@@ -59,28 +58,37 @@ export abstract class AbstractValidator<T> implements Validatable<T> {
 
     validateAsync(input: T): Promise<ValidationResult> {
         return new Promise<ValidationResult>((resolve) => {
-            let promises = new Array<Promise<RuleApplicationOutcome>>();
-
-            this.rules.forEach((element: any) => {
-                let rule = element as ValidationRule<T, any>;
-                promises.push(this.applyRuleAsync(rule, input));
-            });
+            let promises = this.createPromiseForEachRule(input, this.rules);
 
             Promise.all(promises).then((outcomes: RuleApplicationOutcome[]) => {
-                let result = new ValidationResult();
-                outcomes.forEach((outcome: RuleApplicationOutcome) => {
-                    if (outcome.isFailure()) {
-                        result.addFailure(outcome.getValidationFailure());
-                    }
-                });
-                resolve(result);
+                resolve(this.buildValidationResultFrom(outcomes));
             });
         });
+    }
+
+    private createPromiseForEachRule(input: T, rules: ValidationRule<T, any>[]): Array<Promise<RuleApplicationOutcome>> {
+        let promises = new Array<Promise<RuleApplicationOutcome>>();
+
+        rules.forEach((rule: ValidationRule<T, any>) => {
+            promises.push(this.applyRuleAsync(rule, input));
+        });
+
+        return promises;
     }
 
     private applyRuleAsync(rule: ValidationRule<T, any>, input: T): Promise<RuleApplicationOutcome> {
         return new Promise((resolve) => {
             resolve(rule.apply(input));
         });
+    }
+
+    private buildValidationResultFrom(outcomes: RuleApplicationOutcome[]): ValidationResult {
+        let result = new ValidationResult();
+        outcomes.forEach((outcome: RuleApplicationOutcome) => {
+            if (outcome.isFailure()) {
+                result.addFailure(outcome.getValidationFailure());
+            }
+        });
+        return result;
     }
 }
