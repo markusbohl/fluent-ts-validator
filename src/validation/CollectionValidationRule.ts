c9f10d1b215e219d0b27191542b3a0b1e9586c47
Markus Bohl <markus.bohl@gmail.com>
2017-01-12 23:40:57 +0100

    Omit type-information for method parameter

--- src/validation/CollectionValidationRule.ts
+++ src/validation/CollectionValidationRule.ts
@@ -22,7 +22,7 @@ export class CollectionValidationRule<T, TProperty extends Iterable<any>> extend
         return outcome;
     }
 
-    private processElementValidation(input: T, element: TProperty, outcome: RuleApplicationOutcome) {
+    private processElementValidation(input: T, element: any, outcome: RuleApplicationOutcome) {
         if (this.validators.some(validator => validator.isValid(element) === false)) {
             let failure = this.createValidationFailure(input, element);
             this.invokeCallbackWith(failure);
