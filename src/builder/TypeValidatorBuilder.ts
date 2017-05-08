65f6a66946ff9ef6b399fd984223a7c18ef5d6a0
Markus Bohl <markus.bohl@gmail.com>
2017-05-09 00:11:13 +0200

    Add JSDoc

--- src/builder/TypeValidatorBuilder.ts
+++ src/builder/TypeValidatorBuilder.ts
@@ -8,9 +8,29 @@ import {
 } from "./";
 
 export interface TypeValidatorBuilder<T> extends CommonValidatorBuilder<T, any> {
+
+    /**
+     * Checks if a property is of type Array.
+     */
     isArray(): CommonValidatorBuilder<T, Array<any>> & ValidationOptionsBuilder<T>;
+
+    /**
+     * Checks if a property is a real boolean.
+     */
     isBoolean(): CommonValidatorBuilder<T, boolean> & ValidationOptionsBuilder<T>;
+
+    /**
+     * Checks if a property is of type Date.
+     */
     isDate(): DateValidatorBuilder<T> & ValidationOptionsBuilder<T>;
+
+    /**
+     * Checks if a property is a real number.
+     */
     isNumber(): NumberValidatorBuilder<T> & ValidationOptionsBuilder<T>;
+
+    /**
+     * Checks if a property is a real string.
+     */
     isString(): StringValidatorBuilder<T> & StringValidationOptionsBuilder<T>;
 }
