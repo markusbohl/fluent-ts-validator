b58c5177e6a16ab06159fa65e598b49d8c391117
Alex Billingsley <abillingsley@developertown.com>
2017-11-25 21:19:38 -0500

    Allows IsGreaterThanOrEqualTo(0)
    Updates IsGreaterThanOrEqualTo Validator to check if null or undefined similar to IsLessThanOrEqualTo to ensure that zero which is falsy is still compared to the threshold

--- src/validators/number-based/IsGreaterThanOrEqualToValidator.spec.ts
+++ src/validators/number-based/IsGreaterThanOrEqualToValidator.spec.ts
@@ -32,5 +32,29 @@ describe("IsGreaterThanOrEqualToValidator", () => {
 
             expect(result).toBeFalsy();
         });
+
+        describe("when threshold is 0", () => {
+            beforeEach(() => {
+                validator = new IsGreaterThanOrEqualToValidator(0);
+            });
+
+            it("should return true if given number is equal to the threshold value", () => {
+                let result = validator.isValid(0);
+
+                expect(result).toBeTruthy();
+            });
+
+            it("should return true if given number is greater than the threshold value", () => {
+                let result = validator.isValid(1);
+
+                expect(result).toBeTruthy();
+            });
+
+            it("should return false if given number is less than the threshold value", () => {
+                let result = validator.isValid(-1);
+
+                expect(result).toBeFalsy();
+            });
+        });
     });
 });
